1e068da95da252195a4c18f2324fd6684e09b9bb
webfelipemaia <webfelipemaia@gmail.com>
2024-05-30 19:20:06 -0300

    Prepare project to test publication.

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,21 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue-bootstrap-levidade/' : '/',  
+  css: {
+    extract: false
+  },
+  configureWebpack: {
+    output: {
+      libraryExport: 'default',
+      filename: 'levidade.[name].js',
+      chunkFilename: 'levidade.[name].js' 
+    }
+  },
+  chainWebpack: config => {
+    config.entry('app').clear().add('./src/main.js');
+    config.output.filename('levidade.js');
+  }
+};
