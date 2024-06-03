13410bfe76119d65190a1aee6e52d1bb44086504
webfelipemaia <webfelipemaia@gmail.com>
2024-06-03 00:57:58 -0300

    App.vue has been updated.

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template>
   <p class="py-5 my-5 text-center">Levidade Bootstrap component for Vue.</p>
   
-  <leve-alert is-dismissible :is-alert="false" :single-alert="{ id:0, type: 'primary', heading: 'Heading text', text: 'Lorem ipsum dolir', comment: 'sit amet'}"></leve-alert>
-  <leve-alert is-dismissible :is-alert="false"></leve-alert>
 </template>
 
 <script>
