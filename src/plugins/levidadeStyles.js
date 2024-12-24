a03929a9c406d622b3705412724b23b05a419099
webfelipemaia <webfelipemaia@gmail.com>
2024-12-24 03:48:34 -0300

    Added Open Sans font

--- src/plugins/levidadeStyles.js
+++ src/plugins/levidadeStyles.js
@@ -1,3 +1,4 @@
+import "@/assets/custom/theme/font.scss"
 import "@/assets/levidade.scss";
-//import "@/css/levidade.css";
+//import "@/assets/css/levidade.css";
 import "bootstrap-icons/font/bootstrap-icons.css"
