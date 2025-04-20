ffa58b45bcdc616b5a2defa503eaef81690d66e9
webfelipemaia <webfelipemaia@gmail.com>
2025-04-20 16:52:37 -0300

    remove: Deleted LeveOffcanvasHeader component

---
+++ src/examples/ComponentExample.vue
@@ -0,0 +1,52 @@
+<template>  
+  <leve-wrapper>
+    <template #content-top>
+      <section id="introduction">
+      <h3>Levidade UI Kit</h3>
+      <p>Use lindos templates desenvolvidos para agilizar a criação de seu site.</p>
+      <p>Este guia ajudará você a começar a usar o kit de templates, saber como executar, personalizar, atualizar e integrar seu tema!</p>
+      </section>
+    </template>
+  <template #content-middle>
+
+  <section id="description">
+    <h4>Descrição</h4>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quis ratione accusantium eum culpa accusamus, maxime provident 
+      facilis dolores. Iste ullam ab sapiente. Sed voluptate nemo obcaecati expedita necessitatibus illum.</p>
+
+      <br>offcanvas
+
+      
+  </section>
+
+<section id="features">
+<h3>O Que Você Pode Criar com o Levidade UI Kit?</h3>
+
+</section>
+
+<section id="technologies">
+
+
+</section>
+
+  </template>
+
+  <template #aside-right>
+    <leve-fixednav>
+      <template #list-links>
+        <li class="detail-entry detail-h2">
+          <a href="#introduction">Introdução</a>
+        </li>
+        <li class="detail-entry detail-h2"><a href="#description">Description</a></li>
+        <li class="detail-entry detail-h2"><a href="#features">Features</a></li>
+        <li class="detail-entry detail-h2"><a href="#technologies">Technologies</a></li>          
+      </template>
+    </leve-fixednav>
+  </template>
+  </leve-wrapper>
+</template>
+
+<script setup>
+import LeveWrapper from '@/views/layout/LeveWrapper.vue';
+import LeveFixednav from '@/components/LeveFixednav.vue';
+</script>
---
+++ src/views/layout/ContentHeader.vue
@@ -0,0 +1,14 @@
+<template>
+    <header>
+      <LeveOffcanvasTrigger context="mainMenu" />
+      <!-- Resto do header -->
+       
+      <li class="nav-item"><a class="nav-link text-white" href="#">Home</a></li>
+        <li class="nav-item"><a class="nav-link text-white" href="#">About</a></li>
+        <li class="nav-item"><a class="nav-link text-white" href="#">Contact</a></li>
+    </header>
+  </template>
+  
+  <script setup>
+  import LeveOffcanvasTrigger from '@/components/offcanvas/LeveOffcanvasTrigger.vue';
+  </script>
